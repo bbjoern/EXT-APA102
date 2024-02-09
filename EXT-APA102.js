ccaededd6a64c33c7ce6196a374430e310652ba3
Björn Braig <bjoern@oozima.com>
2024-02-09 11:40:47 +0100

    Changed lib to spi-device

--- EXT-APA102.js
+++ EXT-APA102.js
@@ -1,1 +1,28 @@
-Module.register("EXT-APA102",{defaults:{debug:!1},start:function(){},notificationReceived:function(e,t,i){switch(e){case"GA_READY":i.name=="MMM-GoogleAssistant"&&(this.sendSocketNotification("INIT",this.config),this.sendNotification("EXT_HELLO",this.name));break;case"ASSISTANT_LISTEN":this.sendSocketNotification("APA102_COLOR",this.configHelper.player.minVolume);break;case"ASSISTANT_THINK":this.sendSocketNotification("APA102_COLOR",this.configHelper.player.minVolume);break;case"ASSISTANT_REPLY":this.sendSocketNotification("APA102_COLOR",this.configHelper.player.minVolume);break;case"ASSISTANT_ERROR":this.sendSocketNotification("APA102_COLOR",this.configHelper.player.minVolume);break}}});
+Module.register("EXT-APA102", {
+    defaults: {
+        debug: !1
+    },
+    start: function() {},
+    notificationReceived: function(e, t, i) {
+        switch (e) {
+            case "GA_READY":
+                i.name == "MMM-GoogleAssistant" && (this.sendSocketNotification("INIT", this.config), this.sendNotification("EXT_HELLO", this.name));
+                break;
+            case "ASSISTANT_LISTEN":
+                this.sendSocketNotification("APA102_COLOR", {red: 255, green: 255, blue: 255, brightness: 255});
+                break;
+            case "ASSISTANT_THINK":
+                this.sendSocketNotification("APA102_COLOR", {red: 0, green: 0, blue: 255, brightness: 255});
+                break;
+            case "ASSISTANT_REPLY":
+                this.sendSocketNotification("APA102_COLOR", {red: 0, green: 255, blue: 0, brightness: 255});
+                break;
+            case "ASSISTANT_ERROR":
+                this.sendSocketNotification("APA102_COLOR", {red: 255, green: 0, blue: 0, brightness: 255});
+                break;
+            case "ASSISTANT_STANDBY":
+                this.sendSocketNotification("APA102_COLOR", {red: 0, green: 0, blue: 0, brightness: 0});
+                break;
+        }
+    }
+});
--- node_helper.js
+++ node_helper.js
@@ -1,1 +1,75 @@
-"use strict";var NodeHelper=require("node_helper"),rpio=require("rpio");module.exports=NodeHelper.create({start:function(){this.ledProvider=null},socketNotificationReceived:function(e,i){switch(e){case"INIT":console.log("[APA102] EXT-APA102 Version:",require("./package.json").version,"rev:",require("./package.json").rev),this.initialize(i);break;case"APA102_COLOR":console.log("[APA102] Set color");rpio.init({mapping:"gpio",gpiomem:!0}),rpio.open(5,rpio.OUTPUT,rpio.HIGH),rpio.write(5,rpio.HIGH);break}},initialize:async function(e){this.config=e}});
+"use strict";
+var NodeHelper = require("node_helper"),
+    spi = require('spi-device');
+var log = () => { /* do nothing */ };
+module.exports = NodeHelper.create({
+    start: function() {
+        this.ledProvider = null
+    },
+    socketNotificationReceived: function(e, i) {
+        switch (e) {
+            case "INIT":
+                console.log("[APA102] EXT-APA102 Version:", require("./package.json").version, "rev:", require("./package.json").rev), this.initialize(i);
+                break;
+            case "APA102_COLOR":
+                console.log("[APA102] Set color");
+                this.setColor(i)
+                break
+        }
+    },
+    initialize: async function(e) {
+        this.config = e
+        if (e.debug) log = (...args) => { console.log("[APA102]", ...args) }
+    },
+    setColor: function(e) {
+        const apa102 = spi.open(0,
+            0,
+            (err) => {
+                if (err) {
+                    console.error(err);
+                };
+        
+                this.log('Create buffer');
+                this._led_length = 12;
+                this._led_bits =  4 + this._led_length * 4 + this._led_length / 2;
+                this._led_buffer = Buffer.alloc(this._led_bits);
+                for (let i = 0; i < this._led_bits; i++) {
+                    if (i < (this._led_bits - 4))
+                        this._led_buffer[i] = 0x00;
+                    else
+                        this._led_buffer[i] = 255;
+                }
+              
+                this.log('Fill buffer');
+                
+                var red = e.red || Math.floor(Math.random() * (255 - 0 + 1)) + 0;
+                var green = e.green || Math.floor(Math.random() * (255 - 0 + 1)) + 0;
+                var blue = e.blue || Math.floor(Math.random() * (255 - 0 + 1)) + 0;
+                var brightness = e.brightness || 255;
+              
+                for (let i = 0; i < this._led_length; i++) {
+                    var current_led = 4 + (i * 4);
+                    this._led_buffer[current_led + 1] = blue;
+                    this._led_buffer[current_led + 2] = green;
+                    this._led_buffer[current_led + 3] = red;
+                    this._led_buffer[current_led + 0] = brightness;
+                }      
+              
+                const message = [{
+                  byteLength: this._led_bits,
+                  sendBuffer: this._led_buffer,
+                  receiveBuffer: Buffer.alloc(this._led_bits),
+                  speedHz: 20000
+                }];
+              
+                apa102.transfer(message, (err, result) => {
+                    if (err) {
+                        console.error(err);
+                    };
+                })
+        
+            }
+        )
+    }
+
+});
